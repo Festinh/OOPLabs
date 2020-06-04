6d87940f18c23349993cee942e305ab9a504cd0b
Festinh <redodanil@gmail.com>
2020-06-04 18:26:18 +0300

    курсовая работа первый курс

---
+++ Course1WorkProject/TermWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.IO;
+using TermWorkLib;
+
+
+namespace TermWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Website.Run();
+        }
+    }
+
+}
